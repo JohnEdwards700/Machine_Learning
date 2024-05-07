1f2d475373ee2ae6d89d08a70798b10ca9f735f1
John Edwards <johntristanedwards@gmail.com>
2024-05-07 18:18:48 -0500

    update dataset

--- LSTM/train.py
+++ LSTM/train.py
@@ -1,25 +1,25 @@
-from dataset import *
-from LSTM.ltsmmodel import *
+# from dataset import *
+# from LSTM.ltsmmodel import *
 
-model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))
-loss_function = nn.NLLLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.1)
+# model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix))
+# loss_function = nn.NLLLoss()
+# optimizer = optim.SGD(model.parameters(), lr=0.1)
 
-def train(model, loss_fn, training_data, word_to_ix, tag_to_ix, optimizer, epoch=10):
-     for epoch in range(epoch):  # again, normally you would NOT do 300 epochs, it is toy data
-       for sentence, tags in training_data:
-           model.zero_grad()
+# def train(model, loss_fn, training_data, word_to_ix, tag_to_ix, optimizer, epoch=10):
+#      for epoch in range(epoch):  # again, normally you would NOT do 300 epochs, it is toy data
+#        for sentence, tags in training_data:
+#            model.zero_grad()
 
-           # Step 2. Get our inputs ready for the network, that is, turn them into
-           # Tensors of word indices.
-           sentence_in = seq_to_embedding(sentence, word_to_ix)
-           targets = seq_to_embedding(tags, tag_to_ix)
+#            # Step 2. Get our inputs ready for the network, that is, turn them into
+#            # Tensors of word indices.
+#            sentence_in = seq_to_embedding(sentence, word_to_ix)
+#            targets = seq_to_embedding(tags, tag_to_ix)
 
-           # Step 3. Run our forward pass.
-           tag_scores = model(sentence_in)
-           # Step 4. Compute the loss, gradients, and update the parameters by
-           #  calling optimizer.step()
-           loss = loss_fn(tag_scores, targets)
-           print("loss for epoch ", epoch, ":", loss)
-           loss.backward()
-           optimizer.step()
+#            # Step 3. Run our forward pass.
+#            tag_scores = model(sentence_in)
+#            # Step 4. Compute the loss, gradients, and update the parameters by
+#            #  calling optimizer.step()
+#            loss = loss_fn(tag_scores, targets)
+#            print("loss for epoch ", epoch, ":", loss)
+#            loss.backward()
+#            optimizer.step()
--- budget_app/dataset.py
+++ budget_app/dataset.py
@@ -1,3 +1,6 @@
+import torch
+import torch.utils
+import torch.utils.data
 from transformers import AutoTokenizer
 import pandas as pd
 from datasets import Dataset,load_dataset
@@ -6,26 +9,83 @@ ITEM_TAG = "budget_app\Item Description,TagLabel.csv"
 #    Pre-Process Data
 #    Use correct File Path Relative
 #    The CSV file read is in format: item-description, tag-label
-def tokenization(dataset):
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-    tokenized = tokenizer(dataset["item-description"])
-    # print(tokenized)
-    return tokenized    
+   
 
-def pre_process_csv(csvfile):
+# def convert_tensors(batch):
+#     item_description = [item['item-description'] for item in batch]
+#     label = torch.tensor([label_map[item["tag-label"] for item in batch]])
+#     input_ids = torch.tensor([item["input_ids"] for item in batch])
+#     token_type_ids = torch.tensor([item["token_type_ids"] for item in batch])
+#     attention_mask = torch.tensor([item["attention_mask"] for item in batch])
+    
+#     print("Sample batch after tokenization:")
+#     print(f"Description: {item_description[0]}") 
+#     print(f"Input IDs: {input_ids[0]}")  
+#     print(f"Token Type IDs: {token_type_ids[0]}")  
+#     print(f"Attention Mask: {attention_mask[0]}")
+    
+#     return item_description, label, input_ids, token_type_ids, attention_mask
+
+def pre_process_csv(csvfile, label_col = "tag-label"):
+
+    label_map = {}
+    new_label_id = 0
     raw_data = pd.read_csv(f"{csvfile}")
     raw_data = pd.DataFrame(raw_data)
-    raw_data_test = raw_data
+    raw_data["tag-label"] = raw_data[label_col]
+    
+    for i in range(len(raw_data)):
+        label = raw_data.loc[i, "tag-label"]
+        if label not in label_map:
+            print(label)
+            label_map[label] = new_label_id
+            new_label_id += 1
+        raw_data.loc[i, "tag-label"] = label_map[label]
+        # print(label_map[label])
+        
+    print("First few rows with labels: ")
+    print(raw_data.head())
+    raw_data_test = raw_data.copy()
     raw_data_test['tag-label'] = None
     dataset = Dataset.from_pandas(raw_data, split="train")
     datasetTest = Dataset.from_pandas(raw_data_test, split="test")
     # print(dataset) 
     # print(datasetTest)
-
+    def tokenization(dataset):
+        tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        tokenized = tokenizer(dataset["item-description"], padding="max_length", truncation=True)
+        # print(tokenized)
+        return tokenized 
+    
     dataset = dataset.map(tokenization, batched=True)
     # print(dataset)
-    dataset = dataset.set_format(type="torch", columns=['item-description', 'tag-label', 'input_ids', 'token_type_ids', 'attention_mask'])
+    def convert_tensors(batch):
+            item_description = [item['item-description'] for item in batch]
+            label = torch.tensor([label_map [item["tag-label"]] for item in batch])
+            input_ids = torch.tensor([item["input_ids"] for item in batch])
+            token_type_ids = torch.tensor([item["token_type_ids"] for item in batch])
+            attention_mask = torch.tensor([item["attention_mask"] for item in batch])
+            
+            print("Sample batch after tokenization:")
+            print(f"Description: {item_description[0]}") 
+            print(f"Input IDs: {input_ids[0]}")  
+            print(f"Token Type IDs: {token_type_ids[0]}")  
+            print(f"Attention Mask: {attention_mask[0]}")
+            
+            return item_description, label, input_ids, token_type_ids, attention_mask
+    dataset = dataset.set_format(
+        type="torch",
+        columns=['item-description',
+                 'tag-label',
+                 'input_ids',
+                 'token_type_ids',
+                 'attention_mask']
+        )
+    
+    train_dataset = torch.utils.data.DataLoader(dataset, batch_size=32, collate_fn=convert_tensors)
+    test_dataset = torch.utils.data.DataLoader(datasetTest, batch_size=32, collate_fn=convert_tensors)
     print(dataset)
+    return train_dataset, test_dataset
 
 # def alt_pre_process_csv(csvfile):
 #     raw_data = pd.read_csv(f"{csvfile}")
